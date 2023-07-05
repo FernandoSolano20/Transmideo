29b0d7250f671e1a1d2dce8576bf8f25c68686cd
Fernando Solano <fersolano@ex2outcoding.local>
2023-07-04 23:47:01 -0600

    Change data to see more real jeje

--- scripts.js
+++ scripts.js
@@ -78,69 +78,192 @@ for (let index = 1; index < 11; index++) {
 
 
 var i = 1;
-for (let index = 1; index < 51; index++) {
-    log(`INSERT INTO cast_movie VALUES (${i}, ${index}, ${getRandomInt(1, 11)});`);
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_movie VALUES (${i}, ${getRandomInt(1, 6)}, ${index});`);
     i++;
 }
-for (let index = 1; index < 51; index++) {
-    log(`INSERT INTO cast_movie VALUES (${i}, ${index}, ${getRandomInt(11, 21)});`);
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_movie VALUES (${i}, ${getRandomInt(6, 11)}, ${index});`);
     i++;
 }
-for (let index = 1; index < 51; index++) {
-    log(`INSERT INTO cast_movie VALUES (${i}, ${index}, ${getRandomInt(21, 31)});`);
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_movie VALUES (${i}, ${getRandomInt(11, 16)}, ${index});`);
     i++;
 }
-for (let index = 1; index < 51; index++) {
-    log(`INSERT INTO cast_movie VALUES (${i}, ${index}, ${getRandomInt(31, 41)});`);
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_movie VALUES (${i}, ${getRandomInt(16, 21)}, ${index});`);
     i++;
 }
-var i = 1;
-for (let index = 1; index < 51; index++) {
-    log(`INSERT INTO cast_serie VALUES (${i}, ${index}, ${getRandomInt(1, 11)});`);
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_movie VALUES (${i}, ${getRandomInt(21, 26)}, ${index});`);
     i++;
 }
-for (let index = 1; index < 51; index++) {
-    log(`INSERT INTO cast_serie VALUES (${i}, ${index}, ${getRandomInt(11, 21)});`);
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_movie VALUES (${i}, ${getRandomInt(26, 31)}, ${index});`);
     i++;
 }
-for (let index = 1; index < 51; index++) {
-    log(`INSERT INTO cast_serie VALUES (${i}, ${index}, ${getRandomInt(21, 31)});`);
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_movie VALUES (${i}, ${getRandomInt(31, 36)}, ${index});`);
     i++;
 }
-for (let index = 1; index < 51; index++) {
-    log(`INSERT INTO cast_serie VALUES (${i}, ${index}, ${getRandomInt(31, 41)});`);
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_movie VALUES (${i}, ${getRandomInt(36, 41)}, ${index});`);
     i++;
 }
-var i = 1;
-for (let index = 1; index < 51; index++) {
-    log(`INSERT INTO cast_documentary VALUES (${i}, ${index}, ${getRandomInt(1, 11)});`);
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_movie VALUES (${i}, ${getRandomInt(41, 46)}, ${index});`);
     i++;
 }
-for (let index = 1; index < 51; index++) {
-    log(`INSERT INTO cast_documentary VALUES (${i}, ${index}, ${getRandomInt(11, 21)});`);
-    i++;
-}
-for (let index = 1; index < 51; index++) {
-    log(`INSERT INTO cast_documentary VALUES (${i}, ${index}, ${getRandomInt(21, 31)});`);
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_movie VALUES (${i}, ${getRandomInt(46, 51)}, ${index});`);
     i++;
 }
 
+var i = 1;
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_serie VALUES (${i}, ${getRandomInt(1, 6)}, ${index});`);
+    i++;
+}
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_serie VALUES (${i}, ${getRandomInt(6, 11)}, ${index});`);
+    i++;
+}
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_serie VALUES (${i}, ${getRandomInt(11, 16)}, ${index});`);
+    i++;
+}
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_serie VALUES (${i}, ${getRandomInt(16, 21)}, ${index});`);
+    i++;
+}
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_serie VALUES (${i}, ${getRandomInt(21, 26)}, ${index});`);
+    i++;
+}
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_serie VALUES (${i}, ${getRandomInt(26, 31)}, ${index});`);
+    i++;
+}
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_serie VALUES (${i}, ${getRandomInt(31, 36)}, ${index});`);
+    i++;
+}
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_serie VALUES (${i}, ${getRandomInt(36, 41)}, ${index});`);
+    i++;
+}
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_serie VALUES (${i}, ${getRandomInt(41, 46)}, ${index});`);
+    i++;
+}
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_serie VALUES (${i}, ${getRandomInt(46, 51)}, ${index});`);
+    i++;
+}
 
-for (let index = 1; index < 201; index++) {
-    log(`INSERT INTO cast_movie_role VALUES (${index}, ${getRandomInt(1, 3)});`);
+var i = 1;
+for (let index = 1; index < 31; index++) {
+    log(`INSERT INTO cast_documentary VALUES (${i}, ${getRandomInt(1, 6)}, ${index});`);
+    i++;
 }
-for (let index = 1; index < 201; index++) {
-    log(`INSERT INTO cast_movie_role VALUES (${index}, ${getRandomInt(3, 5)});`);
+for (let index = 1; index < 31; index++) {
+    log(`INSERT INTO cast_documentary VALUES (${i}, ${getRandomInt(6, 11)}, ${index});`);
+    i++;
 }
-for (let index = 1; index < 201; index++) {
-    log(`INSERT INTO cast_serie_role VALUES (${index}, ${getRandomInt(1, 3)});`);
+for (let index = 1; index < 31; index++) {
+    log(`INSERT INTO cast_documentary VALUES (${i}, ${getRandomInt(11, 16)}, ${index});`);
+    i++;
 }
-for (let index = 1; index < 201; index++) {
-    log(`INSERT INTO cast_serie_role VALUES (${index}, ${getRandomInt(3, 5)});`);
+for (let index = 1; index < 31; index++) {
+    log(`INSERT INTO cast_documentary VALUES (${i}, ${getRandomInt(16, 21)}, ${index});`);
+    i++;
 }
-for (let index = 1; index < 151; index++) {
-    log(`INSERT INTO cast_documentary_role VALUES (${index}, ${getRandomInt(1, 3)});`);
+for (let index = 1; index < 31; index++) {
+    log(`INSERT INTO cast_documentary VALUES (${i}, ${getRandomInt(21, 26)}, ${index});`);
+    i++;
 }
-for (let index = 1; index < 151; index++) {
-    log(`INSERT INTO cast_documentary_role VALUES (${index}, ${getRandomInt(3, 5)});`);
+for (let index = 1; index < 31; index++) {
+    log(`INSERT INTO cast_documentary VALUES (${i}, ${getRandomInt(26, 31)}, ${index});`);
+    i++;
+}
+for (let index = 1; index < 31; index++) {
+    log(`INSERT INTO cast_documentary VALUES (${i}, ${getRandomInt(31, 36)}, ${index});`);
+    i++;
+}
+for (let index = 1; index < 31; index++) {
+    log(`INSERT INTO cast_documentary VALUES (${i}, ${getRandomInt(36, 41)}, ${index});`);
+    i++;
+}
+for (let index = 1; index < 31; index++) {
+    log(`INSERT INTO cast_documentary VALUES (${i}, ${getRandomInt(41, 46)}, ${index});`);
+    i++;
+}
+for (let index = 1; index < 31; index++) {
+    log(`INSERT INTO cast_documentary VALUES (${i}, ${getRandomInt(46, 51)}, ${index});`);
+    i++;
+}
+
+var i = 1;
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_movie_role VALUES (${i}, 3);`);
+    i++;
+}
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_movie_role VALUES (${i}, 4);`);
+    i++;
+}
+for (let index = 81; index < 241; index++) {
+    log(`INSERT INTO cast_movie_role VALUES (${i}, ${getRandomInt(1, 3)});`);
+    i++;
+}
+for (let index = 241; index < 401; index++) {
+    log(`INSERT INTO cast_movie_role VALUES (${i}, ${getRandomInt(1, 3)});`);
+    i++;
+}
+for (let index = 1; index < 11; index++) {
+    log(`INSERT INTO cast_movie_role VALUES (${getRandomInt(81, 401)}, 3);`);
+}
+
+
+var i = 1;
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_serie_role VALUES (${i}, 3);`);
+    i++;
+}
+for (let index = 1; index < 41; index++) {
+    log(`INSERT INTO cast_serie_role VALUES (${i}, 4);`);
+    i++;
+}
+for (let index = 81; index < 241; index++) {
+    log(`INSERT INTO cast_serie_role VALUES (${i}, ${getRandomInt(1, 3)});`);
+    i++;
+}
+for (let index = 241; index < 401; index++) {
+    log(`INSERT INTO cast_serie_role VALUES (${i}, ${getRandomInt(1, 3)});`);
+    i++;
+}
+for (let index = 1; index < 11; index++) {
+    log(`INSERT INTO cast_serie_role VALUES (${getRandomInt(81, 401)}, 3);`);
+}
+
+
+var i = 1;
+for (let index = 1; index < 31; index++) {
+    log(`INSERT INTO cast_documentary_role VALUES (${i}, 3);`);
+    i++;
+}
+for (let index = 1; index < 31; index++) {
+    log(`INSERT INTO cast_documentary_role VALUES (${i}, 4);`);
+    i++;
+}
+for (let index = 61; index < 181; index++) {
+    log(`INSERT INTO cast_documentary_role VALUES (${i}, ${getRandomInt(1, 3)});`);
+    i++;
+}
+for (let index = 181; index < 301; index++) {
+    log(`INSERT INTO cast_documentary_role VALUES (${i}, ${getRandomInt(1, 3)});`);
+    i++;
+}
+for (let index = 1; index < 11; index++) {
+    log(`INSERT INTO cast_documentary_role VALUES (${getRandomInt(61, 301)}, 3);`);
 }
