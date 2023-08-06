4ebe9e5fb61a0a9113ca227ee3c1b72a7337cf66
Fernando Solano <fersolano@ex2outcoding.local>
2023-08-06 16:23:12 -0600

    Trigger validate download and reproduction

--- transactions/testing.sql
+++ transactions/testing.sql
@@ -7,13 +7,13 @@ Begin
   --Insert_Client('Fernando', 'Solano', 'Hatillo', '8472-7296', to_date('31/07/00','DD/MM/RR'), 'fesolano@gmail.com', 1, 1); 
   --Insert_Membership(10000, 51); 
 
-  --Insert_Reproduction_Movie(41, 51);
-  --Insert_Reproduction_Serie(81, 51);
-  --Insert_Reproduction_Doc(61, 51);
+  --Insert_Reproduction_Movie(41, 34);
+  --Insert_Reproduction_Serie(81, 34);
+  --Insert_Reproduction_Doc(61, 34);
 
-  --Insert_Download_Movie(41, 51);
-  --Insert_Download_Serie(81, 51);
-  --Insert_Download_Doc(61, 51);
+  --Insert_Download_Movie(41, 34);
+  --Insert_Download_Serie(81, 34);
+  --Insert_Download_Doc(61, 34);
 
   --Add_Movie_To_Saga(5, 41);
   --Add_Serie_To_Macroserie(5, 41);
---
+++ transactions/validate_download_serie.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE TRIGGER insert_download_serie_trg
+BEFORE INSERT ON download_serie
+FOR EACH ROW
+DECLARE
+  CURSOR client_cursor IS
+  SELECT MAX(expiration_day)
+  FROM membership
+  WHERE client_id = :NEW.client_id;
+  max_expiration_day DATE;
+BEGIN
+  OPEN client_cursor;
+  FETCH client_cursor INTO max_expiration_day;
+  CLOSE client_cursor;
+
+  IF max_expiration_day < SYSDATE THEN
+    RAISE_APPLICATION_ERROR(-20502,'Su membresia ha expirado.');
+  END IF;
+END;
+/
