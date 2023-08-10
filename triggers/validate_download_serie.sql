381da832854766039733d12afccab1d70a069af0
Fernando Solano <fersolano@ex2outcoding.local>
2023-08-10 15:08:47 -0600

    Add status in the trigger when the video download or reproduction is requested

--- triggers/validate_download_serie.sql
+++ triggers/validate_download_serie.sql
@@ -3,16 +3,25 @@ BEFORE INSERT ON download_serie
 FOR EACH ROW
 DECLARE
   CURSOR client_cursor IS
-  SELECT MAX(expiration_day)
-  FROM membership
-  WHERE client_id = :NEW.client_id;
+  SELECT MAX(m.expiration_day), c.account_status_id
+  FROM membership m
+  INNER JOIN client c ON c.id = m.client_id
+  WHERE client_id = :NEW.client_id
+  GROUP BY c.account_status_id;
+
   max_expiration_day DATE;
+  account_status NUMBER;
 BEGIN
   OPEN client_cursor;
-  FETCH client_cursor INTO max_expiration_day;
+  FETCH client_cursor INTO max_expiration_day, account_status;
   CLOSE client_cursor;
 
   IF max_expiration_day < SYSDATE THEN
+    IF account_status = 1 THEN
+      UPDATE client
+        SET account_status_id = 2
+        WHERE id = :NEW.client_id;
+    END IF;
     RAISE_APPLICATION_ERROR(-20502,'Su membresia ha expirado.');
   END IF;
 END;
